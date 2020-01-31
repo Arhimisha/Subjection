726baa08cca56f6db38f534f3374d253030bf152
Mikhail Arkhipov <miklpostbox@gmail.com>
2020-01-31 21:13:38 +0300

    Add save and view for subject

---
+++ src/main/java/com/arhimisha/intech/controllers/SubjectController.java
@@ -0,0 +1,79 @@
+package com.arhimisha.intech.controllers;
+
+import com.arhimisha.intech.domain.Subject;
+import com.arhimisha.intech.domain.User;
+import com.arhimisha.intech.services.SubjectService;
+import com.arhimisha.intech.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.GregorianCalendar;
+import java.util.Optional;
+
+@Controller
+@RequestMapping(value = "/subject")
+public class SubjectController {
+
+    private final SubjectService subjectService;
+    private final UserService userService;
+
+    @Autowired
+    public SubjectController(SubjectService subjectService, UserService userService) {
+        this.subjectService = subjectService;
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/{id:^[0-9]+$}")
+    public ModelAndView getSubject(@PathVariable String id) {
+        ModelAndView model = new ModelAndView("subject");
+
+        long longId = 0L;
+        try {
+            longId = Long.parseLong(id);
+        } catch (NumberFormatException e) {
+            //todo переделать все RuntimeException в открытие страниц с ошибкой 404 кроме страницы регистрации
+            throw new RuntimeException("Id in url is not a number");
+        }
+
+        Optional<Subject> subject = subjectService.loadById(longId);
+        if (subject.isEmpty() || subject.get().isDeleted()) {
+            throw new RuntimeException("Subject is not found");
+        }
+        model.addObject("name", subject.get().getName());
+        model.addObject("description", subject.get().getDescription());
+        model.addObject("author", subject.get().getAuthor().getFullName());
+        model.addObject("creationDate", subject.get().getCreationDate().getTime());
+        return model;
+    }
+
+    @PostMapping(value = "/create")
+    public String create(
+            @RequestParam(name = "name", required = true) String name,
+            @RequestParam(name = "description", defaultValue = "", required = false) String description,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        if (name.isBlank()) {
+            throw new RuntimeException("Subject Name is empty");
+        }
+
+        final User user = (User) userService.loadUserByUsername(userDetails.getUsername());
+        if (user == null) {
+            throw new RuntimeException("User " + userDetails.getUsername() + " is not exist");
+        }
+        Subject subject = new Subject(
+                0L,
+                name,
+                description,
+                user,
+                new GregorianCalendar(),
+                false
+        );
+        subject = subjectService.save(subject);
+        return "redirect:/subject/" + subject.getId();
+    }
+
+}
--- src/main/java/com/arhimisha/intech/services/SubjectService.java
+++ src/main/java/com/arhimisha/intech/services/SubjectService.java
@@ -1,10 +1,12 @@
 package com.arhimisha.intech.services;
 
+import com.arhimisha.intech.domain.Subject;
 import com.arhimisha.intech.repository.SubjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -14,6 +16,15 @@ public class SubjectService {
     @Autowired
     public SubjectService(SubjectRepository subjectRepository) {
         this.subjectRepository = subjectRepository;
+    }
+
+    public Subject save(Subject subject) {
+        return subjectRepository.save(subject);
+    }
+
+    public Optional<Subject> loadById(long id) {
+        return subjectRepository.findById(id);
+
     }
 
 }
