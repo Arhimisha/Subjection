e8ffc997310ab497bc194d7dac47b46cf612a261
Mikhail Arkhipov <miklpostbox@gmail.com>
2020-01-31 19:42:46 +0300

    add try-catch in MainController

--- src/main/java/com/arhimisha/intech/controllers/MainController.java
+++ src/main/java/com/arhimisha/intech/controllers/MainController.java
@@ -5,6 +5,7 @@ import com.arhimisha.intech.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,9 +24,14 @@ public class MainController {
     public ModelAndView main(@AuthenticationPrincipal UserDetails userDetails) {
         final ModelAndView modelAndView = new ModelAndView("main");
         if(userDetails != null){
-
-            User user = (User) userService.loadUserByUsername(userDetails.getUsername());
-            modelAndView.addObject("userFullName",user.getFullName());
+            try {
+                final UserDetails user =  userService.loadUserByUsername(userDetails.getUsername());
+                if(user != null && user instanceof User){
+                    modelAndView.addObject("userFullName",((User)user).getFullName());
+                }
+            } catch (UsernameNotFoundException e) {
+                throw new RuntimeException(e.getMessage());
+            }
         }
         return modelAndView;
     }
